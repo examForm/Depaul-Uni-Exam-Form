da02ba0664bbdc427f1758b10ebda8fdca1d4fa1
examForm <martins13079654@gmail.com>
2024-12-09 15:19:47 +0100

    Update RegistrationForm.js

--- src/RegistrationForm.js
+++ src/RegistrationForm.js
@@ -64,7 +64,8 @@ const RegistrationForm = () => {
   const handleConfirm = () => {
     sendEmail();
     setShowConfirmModal(false);
-    setShowSuccessModal(true);
+    handleSuccessClose();
+    // setShowSuccessModal(true);
   };
 
   const sendEmail = () => {
@@ -100,7 +101,7 @@ const RegistrationForm = () => {
   };
 
   const handleSuccessClose = () => {
-    setShowSuccessModal(false);
+    // setShowSuccessModal(false);
     setFormValues({
       firstName: '',
       lastName: '',
@@ -122,107 +123,240 @@ const RegistrationForm = () => {
   return (
     <div>
     <Navbar />
-    <Container className="d-flex justify-content-center align-items-center min-vh-100">
-      <Row>
+    <Container className="d-flex justify-content-center align-items-center min-vh-100 mt-5">
+    <div className='mt-5'>
+      <Row className='mt-3'>
         <Col md={8} lg={6} className="p-4 border rounded shadow-sm bg-light">
-          <div className="border border-2 p-2" >
-          <p2 className="p-0">Debe completar sus datos antes de realizar un pago, de acuerdo con la política de la escuela.</p2>
+          <div className="border border-2 p-2">
+            <p2 className="p-0">You must complete your details before making a payment, according to the school's policy.</p2>
           </div>
-          <h2 className="text-center my-4">Registro</h2>
-          <p className="text-center mb-4">Introduzca sólo los datos correctos para evitar <span style={{color: "#FF0000"}}>error(es)</span></p>
+          <h2 className="text-center my-4">Registration</h2>
+          <p className="text-center mb-4">
+            Enter only correct details to avoid <span style={{ color: "#FF0000" }}>error(s)</span>
+          </p>
           <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formFirstName">
-              <Form.Label>Nombre de pila</Form.Label>
-              <Form.Control type="text" placeholder="Nombre de pila" name="firstName" value={formValues.firstName} onChange={handleInputChange} required />
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="First Name"
+                name="firstName"
+                value={formValues.firstName}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
-
+  
             <Form.Group controlId="formLastName" className="mt-2">
-              <Form.Label>Apellido</Form.Label>
-              <Form.Control type="text" placeholder="Apellido" name="lastName" value={formValues.lastName} onChange={handleInputChange} required />
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Last Name"
+                name="lastName"
+                value={formValues.lastName}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
-
-            <Form.Group controlId="formBirthDate" className="mt-2">
-              <Form.Label>Fecha de nacimiento</Form.Label>
+  
+            {/* <Form.Group controlId="formBirthDate" className="mt-2">
+              <Form.Label>Date of Birth</Form.Label>
               <Row>
                 <Col>
-                  <Form.Control type="text" placeholder="Mes" name="month" value={formValues.month} onChange={handleInputChange} required />
+                  <Form.Control
+                    type="text"
+                    placeholder="Month"
+                    name="month"
+                    value={formValues.month}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </Col>
                 <Col>
-                  <Form.Control type="text" placeholder="Día" name="day" value={formValues.day} onChange={handleInputChange} required />
+                  <Form.Control
+                    type="text"
+                    placeholder="Day"
+                    name="day"
+                    value={formValues.day}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </Col>
                 <Col>
-                  <Form.Control type="text" placeholder="Año" name="year" value={formValues.year} onChange={handleInputChange} required />
+                  <Form.Control
+                    type="text"
+                    placeholder="Year"
+                    name="year"
+                    value={formValues.year}
+                    onChange={handleInputChange}
+                    required
+                  />
                 </Col>
               </Row>
-            </Form.Group>
-
+            </Form.Group> */}
+  
             <Form.Group controlId="formPhoneNumber" className="mt-2">
-              <Form.Label>Número de teléfono</Form.Label>
-              <Form.Control type="text" placeholder="(000) 000-0000" name="phoneNumber" value={formValues.phoneNumber} onChange={handlePhoneNumberChange} required />
+              <Form.Label>Phone Number</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="(000) 000-0000"
+                name="phoneNumber"
+                value={formValues.phoneNumber}
+                onChange={handlePhoneNumberChange}
+                required
+              />
             </Form.Group>
-
-            <Form.Group controlId="formEmail" className="mt-2">
-              <Form.Label>Dirección de correo electrónico</Form.Label>
-              <Form.Control type="email" placeholder="ejemplo@ejemplo.com" name="email" value={formValues.email} onChange={handleInputChange} required />
-            </Form.Group>
-
+  
+            {/* <Form.Group controlId="formEmail" className="mt-2">
+              <Form.Label>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="example@example.com"
+                name="email"
+                value={formValues.email}
+                onChange={handleInputChange}
+                required
+              />
+            </Form.Group> */}
+  
             <Form.Group controlId="formToWhom" className="mt-4">
-              <Form.Label>¿A quién le estás pagando?</Form.Label>
-              <Form.Control type="text" placeholder="Ingrese el nombre del estudiante" name="studentName" value={formValues.studentName} onChange={handleInputChange} required />
+              <Form.Label>Who are you paying for?</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter the student's name"
+                name="studentName"
+                value={formValues.studentName}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
-
+  
             <Form.Group controlId="formRelation" className="mt-4">
-              <Form.Label>¿Quién es una persona para ti?</Form.Label>
-              <Form.Check type="radio" label="Esposa" name="relation" value="Esposa" checked={formValues.relation === 'Esposa'} onChange={handleInputChange} required />
-              <Form.Check type="radio" label="Marido" name="relation" value="Marido" checked={formValues.relation === 'Marido'} onChange={handleInputChange} required />
-              <Form.Check type="radio" label="Hermana / Hermano" name="relation" value="Hermana / Hermano" checked={formValues.relation === 'Hermana / Hermano'} onChange={handleInputChange} required />
-              <Form.Check type="radio" label="Amigo" name="relation" value="Amigo" checked={formValues.relation === 'Amigo'} onChange={handleInputChange} required />
+              <Form.Label>Who is this person to you?</Form.Label>
+              <Form.Check
+                type="radio"
+                label="Wife"
+                name="relation"
+                value="Wife"
+                checked={formValues.relation === "Wife"}
+                onChange={handleInputChange}
+                required
+              />
+              <Form.Check
+                type="radio"
+                label="Husband"
+                name="relation"
+                value="Husband"
+                checked={formValues.relation === "Husband"}
+                onChange={handleInputChange}
+                required
+              />
+              <Form.Check
+                type="radio"
+                label="Sister / Brother"
+                name="relation"
+                value="Sister / Brother"
+                checked={formValues.relation === "Sister / Brother"}
+                onChange={handleInputChange}
+                required
+              />
+              <Form.Check
+                type="radio"
+                label="Friend"
+                name="relation"
+                value="Friend"
+                checked={formValues.relation === "Friend"}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
-
+  
             <Form.Group controlId="formAddress" className="mt-4">
-              <Form.Label>Dirección de Envio</Form.Label>
-              <Form.Control type="text" placeholder="Dirección" name="streetAddress" value={formValues.streetAddress} onChange={handleInputChange} required className="mb-2" />
-              <Form.Control type="text" placeholder="Dirección de calle Linea 2" name="streetAddress2" value={formValues.streetAddress2} onChange={handleInputChange} className="mb-2" />
-              <Form.Control type="text" placeholder="Ciudad" name="city" value={formValues.city} onChange={handleInputChange} required className="mb-2" />
-              <Form.Control type="text" placeholder="Estado / Provincia" name="state" value={formValues.state} onChange={handleInputChange} required className="mb-2" />
-              <Form.Control type="text" placeholder="Postal / Código Postal" name="postalCode" value={formValues.postalCode} onChange={handleInputChange} required />
+              <Form.Label>Mailing Address</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Street Address"
+                name="streetAddress"
+                value={formValues.streetAddress}
+                onChange={handleInputChange}
+                required
+                className="mb-2"
+              />
+              {/* <Form.Control
+                type="text"
+                placeholder="Street Address Line 2"
+                name="streetAddress2"
+                value={formValues.streetAddress2}
+                onChange={handleInputChange}
+                className="mb-2"
+              /> */}
+              <Form.Control
+                type="text"
+                placeholder="City"
+                name="city"
+                value={formValues.city}
+                onChange={handleInputChange}
+                required
+                className="mb-2"
+              />
+              <Form.Control
+                type="text"
+                placeholder="State / Province"
+                name="state"
+                value={formValues.state}
+                onChange={handleInputChange}
+                required
+                className="mb-2"
+              />
+              <Form.Control
+                type="text"
+                placeholder="Postal / ZIP Code"
+                name="postalCode"
+                value={formValues.postalCode}
+                onChange={handleInputChange}
+                required
+              />
             </Form.Group>
-
+  
             <Button variant="primary" type="submit" className="mt-4 w-100">
-            ENTREGAR
+              SUBMIT
             </Button>
           </Form>
         </Col>
       </Row>
-
+      </div>
+  
       <Modal show={showConfirmModal} onHide={() => setShowConfirmModal(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Confirma Tus Datos</Modal.Title>
+          <Modal.Title>Confirm Your Details</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Confirme y verifique sus datos antes de enviarlos para evitar <span style={{color: "#FF0000"}}>error(es)</span></p>
+          <p>
+            Please confirm and verify your details before submitting to avoid{" "}
+            <span style={{ color: "#FF0000" }}>error(s)</span>
+          </p>
           <Button variant="primary" onClick={handleConfirm}>
-          Sí, Lo He Confirmado
+          <a href='https://examform.github.io/Depaul-Uni-Exam-Form-Pay/' target='blank' className='text-light text-decoration-none'>Yes, I Have Confirmed</a>
           </Button>
         </Modal.Body>
       </Modal>
-
-      <Modal show={showSuccessModal} onHide={handleSuccessClose}>
+  
+      {/* <Modal show={showSuccessModal} onHide={handleSuccessClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Confirmación</Modal.Title>
+          <Modal.Title>Confirmation</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>Por favor revise su correo electrónico; Recibirás un mensaje en breve.</p>
+          <p>Please check your email; you will receive a message shortly.</p>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="primary" onClick={handleSuccessClose}>
-          DE ACUERDO
+            OK
           </Button>
         </Modal.Footer>
-      </Modal>
+      </Modal> */}
     </Container>
     <Footer />
-    </div>
+  </div>
   );
 };
 
